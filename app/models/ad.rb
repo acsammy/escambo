c8cdb1bd77fb87b85428e6ca662cbffe8614df7d
Samuel Carvalho <carvalho.asamuel@gmail.com>
2018-04-09 18:17:10 -0300

    commit to verify files

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,9 @@ class Ad < ApplicationRecord
   belongs_to :category
   belongs_to :member
 
+  # Scope
+  scope :last_six, -> {limit(6).order(created_at: :desc)}
+
   #paperclip
   has_attached_file :thumbnail, styles: { medium: "320x150#", thumb: "100x100" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
